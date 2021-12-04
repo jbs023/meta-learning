8911e21497f0f43e610c31aaa512f0bc415377ea
Joshua R Bhagat Smith <bhagatsj@oregonstate.edu>
2021-12-03 22:33:08 -0800

    Clean up the github

--- src/visualize_loss.py
+++ src/visualize_loss.py
@@ -3,6 +3,14 @@ import pandas as pd
 import argparse
 import os
 
+label_dict = {
+    "siamese_final_loss_cnn_30k_v2.csv" : "CNN",
+    "siamese_final_loss_cnn_d_30k_v2.csv" : "CNN + Dist",
+    "siamese_final_loss_ff_30k_v2.csv" : "FFNN",
+    "siamese_final_loss_ff_dist_30k_v2.csv" : "FFNN + Dist",
+}
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Runs experiments for the swarm consensus simulation.')
@@ -10,11 +18,12 @@ if __name__ == "__main__":
                         help='Path to directory with csv file')
 
     args = parser.parse_args()
+    full_dir_path = os.path.abspath(args.dirpath)
     fig = plt.figure(figsize=(10, 10))
-    files = [f for f in os.listdir(args.dirpath) if os.path.isfile(f)]
+    files = [f for f in os.listdir(full_dir_path)]
     for f in files:
         if ".csv" in f:
-            df = pd.read_csv(f)
+            df = pd.read_csv(os.path.join(full_dir_path, f))
             plt.plot(df.index, df["Accuracy"], label=f)
             # plt.plot(df.index, df["Loss"], label=f)
 
